7a0dc34b80c14435a3789b53b1369589db54edfb
Henrique Zanchi <henriquecarr@gmail.com>
2026-05-12 18:35:08 -0300

    Claude 7.1

--- src/constants/bookMetrics.js
+++ src/constants/bookMetrics.js
@@ -1,1 +1,16 @@
-export { translateCategory, CATEGORY_MAP } from './ranks';
+export const translateCategory = (category) => {
+  if (!category) return 'Filosofia';
+  return category
+    .replace(/Religion/g, 'Religião')
+    .replace(/Philosophy/g, 'Filosofia')
+    .replace(/Fiction/g, 'Ficção')
+    .replace(/History/g, 'História')
+    .replace(/Psychology/g, 'Psicologia')
+    .replace(/Science/g, 'Ciência')
+    .replace(/Self-Help/g, 'Autoajuda')
+    .replace(/Body, Mind & Spirit/g, 'Espiritualidade')
+    .replace(/Biography & Autobiography/g, 'Biografia')
+    .replace(/Literary Collections/g, 'Literatura')
+    .replace(/Poetry/g, 'Poesia')
+    .replace(/Social Science/g, 'Ciências Sociais');
+};
